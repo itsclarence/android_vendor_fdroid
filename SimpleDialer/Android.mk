16a6391be300668243bfdfd55bf16b660c8684b1
itsclarence <postclarence@gmail.com>
2021-03-23 11:41:51 +0100

    Update Android.mk

--- SimpleDialer/Android.mk
+++ SimpleDialer/Android.mk
@@ -1,1 +1,18 @@
 
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := SimpleDialer
+
+LOCAL_SRC_FILES := com.simplemobiletools.dialer_21.apk
+
+LOCAL_MODULE_CLASS := APPS
+
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_CERTIFICATE := PRESIGNED
+
+include $(BUILD_PREBUILT)
